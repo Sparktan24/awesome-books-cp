08e3953b857f610bb3cca80021a856d2088a6aff
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-10-24 17:50:30 -0400

    Complete JS local storage

--- app.js
+++ app.js
@@ -36,3 +36,44 @@ class UI {
     document.querySelector('#author').value = '';
   }
 }
+
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  const book = new Book(title, author);
+
+  UI.addBookList(book);
+
+  Store.addBook(book);
+
+  console.log(book);
+
+  UI.clearFields();
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+});
